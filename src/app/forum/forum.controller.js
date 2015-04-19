26addeadf7f5663f48a30be04f159b413f11c3ec
John Fadich <jofad4@gmail.com>
2015-04-18 18:03:02 -0700

    new line fix

---
+++ src/app/forum/forum.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('inspinia')
+  .controller('ForumCtrl', function ($scope, $http) {
+        $scope.threads = [];
+        this.userName = 'John Fadich';
+        this.helloText = 'Welcome to the Forum';
+        this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects.';
+
+        $http.get('http://myfamily.dev/forum?with=owner').
+            success(function(threads) {
+                $scope.threads = threads.data;console.log(threads);
+            }).
+            error(function(data){
+                console.log(data);
+            });
+    });
